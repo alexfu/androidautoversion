8a8bcd7f89d64c3788c11159b046aba9f7da5389
Alex Fu <alex.fu914@gmail.com>
2016-09-06 12:49:56 -0400

    Use version file path as pattern

--- src/main/groovy/com/github/alexfu/AndroidAutoVersionPlugin.groovy
+++ src/main/groovy/com/github/alexfu/AndroidAutoVersionPlugin.groovy
@@ -1,7 +1,6 @@
 package com.github.alexfu
 
 import org.ajoberstar.grgit.Grgit
-import org.gradle.api.GradleException
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.Task
@@ -84,7 +83,7 @@ class AndroidAutoVersionPlugin implements Plugin<Project> {
             def git = Grgit.open(project.rootDir)
 
             // Add changes
-            git.add(update: true, patterns: ["config/version"])
+            git.add(update: true, patterns: [extension.versionFile.path])
 
             // Commit
             git.commit(message: "Update version")
